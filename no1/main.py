4cd869a55cbb0b1f0a1480a5a560f4ea22581758
WDTX1402 <phatthadonsornplang@gmail.com>
2024-12-27 16:58:40 +0700

    shrek

--- no1/main.py
+++ no1/main.py
@@ -7,7 +7,7 @@ class Simple_drawing_window(QWidget):
     def __init__(self):
         QWidget.__init__(self,None)
         self.setWindowTitle("Simple GitHub Drawing")
-        self.rabbit = QPixmap("images/rabbit.png")
+        self.rabbit = QPixmap("images/shronk.png")
 
     def paintEvent(self, e):
         p = QPainter()
@@ -24,14 +24,15 @@ class Simple_drawing_window(QWidget):
         p.setBrush(QColor(255,127,0))
         p.drawPie(50,150,100,100,0,180*16)
 
-        p.drawPolygon([QPoint(50,200),QPoint(150,200),QPoint(100,400),])
+        p.drawPolygon([QPoint(50,200),QPoint(150,200),QPoint(100,400)])
         p.drawPixmap(QRect(200,100,320,320), self.rabbit)
         p.end()
 
-    def main():
-        app = QApplication(sys.argv)
-        w = Simple_drawing_window()
-        w.show()
+def main():
+    app = QApplication(sys.argv)
+    w = Simple_drawing_window()
+    w.show()
+    return app.exec()
 
-    if __name__ == "__main__":
-        sys.exit(main())
+if __name__ == "__main__":
+    sys.exit(main())
